ecca9736379242f8ac93c0b4e8ce4b061fb711f3
ramesh-cloud <ramesh_dhayalan@outlook.com>
2020-06-06 20:59:20 +0530

    yes

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gitlab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
